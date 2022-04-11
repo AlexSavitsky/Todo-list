04776e1ab6c70b6483b7bbcf45ee34e742e9f70c
Your Name <savasavitsky@gmail.com>
2022-04-11 19:56:48 +0300

    Refactoring code && add style empty inputs

--- js/main.js
+++ js/main.js
@@ -7,7 +7,15 @@ const submitTaskButton = document.getElementById("createTaskButton");
 const newTaskInput = document.getElementById("newTaskInput");
 
 const secondTbody = document.getElementById("secondTbody");
-let dropdownButtons = [];
+
+const priorities = {
+  TODO: "Todo",
+  IN_PROGRESS: "In progress",
+  DONE: "Done",
+  CHANGE_PRIORITY: "Change priority",
+};
+
+let dropdownButtons;
 
 //Creating an array of tasks and populating the created tasks from local storage
 let tasks;
@@ -16,22 +24,24 @@ let tasks;
   : (tasks = JSON.parse(localStorage.getItem("tasks")));
 
 //Tasks constructor
-function Task(description, id) {
-  this.id = id;
-  this.description = description;
-  this.priority = "Change priority";
+class Task {
+  constructor(description, id) {
+    this.id = id;
+    this.description = description;
+    this.priority = "Change priority";
+  }
 }
 
 //Constructor creating a new task in the task table
 const createTemplate = (task, index) => {
-  let newRow = secondTbody.insertRow(-1);
-  let column = newRow.insertCell(0);
-  let column1 = newRow.appendChild(document.createElement("td"));
-  let column2 = newRow.appendChild(document.createElement("td"));
-  let column3 = newRow.appendChild(document.createElement("td"));
+  const newRow = secondTbody.insertRow(-1);
+  const column = newRow.insertCell(0);
+  const column1 = newRow.appendChild(document.createElement("td"));
+  const column2 = newRow.appendChild(document.createElement("td"));
+  const column3 = newRow.appendChild(document.createElement("td"));
   tasks[index].id = index + 1;
   column.innerHTML = task.id;
-  column1.innerHTML = `<div type="button" onclick="editTaskDescription(${index})">${task.description}</div>`;
+  column1.innerHTML = `<div name="description" type="button" onclick="editTaskDescription(${index})">${task.description}</div>`;
   column2.innerHTML = `<button
         class="${updatePriority(index)}"
         type="button"
@@ -83,12 +93,14 @@ const updateLocal = () => {
 
 //Closed hiden form and add new tusk
 submitTaskButton.addEventListener("click", () => {
-  tasks.push(new Task(newTaskInput.value, tasks.length));
-  newTaskInput.value = "";
-  popup.classList.remove("popup_open");
-
-  updateLocal();
-  fillTodoList();
+  newTaskInput.value === ""
+    ? wrongTaskInput(newTaskInput)
+    : (tasks.push(new Task(newTaskInput.value, tasks.length)),
+      (newTaskInput.value = ""),
+      newTaskInput.classList.remove("wrongInput"),
+      popup.classList.remove("popup_open"),
+      updateLocal(),
+      fillTodoList());
 });
 
 //Change priority with dropdown button
@@ -104,16 +116,16 @@ const changePriority = (index, priority) => {
 //Update classes(styles) dropdown buttons
 const updatePriority = (index) => {
   const doneRow = document.getElementsByTagName("tr");
-  if (tasks[index].priority == "Todo") {
+  if (tasks[index].priority === priorities.TODO) {
     doneRow[index + 1].classList.remove("done");
     return "btn btn-info dropdown-toggle";
-  } else if (tasks[index].priority == "In progress") {
+  } else if (tasks[index].priority === priorities.IN_PROGRESS) {
     doneRow[index + 1].classList.remove("done");
     return "btn btn-warning dropdown-toggle";
-  } else if (tasks[index].priority == "Done") {
+  } else if (tasks[index].priority === priorities.DONE) {
     doneRow[index + 1].classList.add("done");
     return "btn btn-success dropdown-toggle";
-  } else if (tasks[index].priority == "Change priority") {
+  } else if (tasks[index].priority === priorities.CHANGE_PRIORITY) {
     return "btn btn-secondary dropdown-toggle";
   }
 
@@ -139,13 +151,15 @@ const deleteTask = (index) => {
 //FIlter tasks (change priority -> in progress -> todo -> done)
 const filterDoneTasks = () => {
   const changePriorityTasks =
-    tasks.length && tasks.filter((item) => item.priority == "Change priority");
+    tasks.length &&
+    tasks.filter((item) => item.priority === priorities.CHANGE_PRIORITY);
   const inProgressTasks =
-    tasks.length && tasks.filter((item) => item.priority == "In progress");
+    tasks.length &&
+    tasks.filter((item) => item.priority === priorities.IN_PROGRESS);
   const toDoTasks =
-    tasks.length && tasks.filter((item) => item.priority == "Todo");
+    tasks.length && tasks.filter((item) => item.priority === priorities.TODO);
   const doneTasks =
-    tasks.length && tasks.filter((item) => item.priority == "Done");
+    tasks.length && tasks.filter((item) => item.priority === priorities.DONE);
   tasks = [
     ...changePriorityTasks,
     ...inProgressTasks,
@@ -159,21 +173,34 @@ const editTaskDescription = (index) => {
   const doneRow = document.getElementsByTagName("tr");
   const descriptionColumn = doneRow[index + 1].getElementsByTagName("td");
   descriptionColumn[1].innerHTML = `
-  <form  onchange="updateDescription(${index}, this)">
-  <input  name="newDescription" value="${tasks[index].description}">
+  <form  onsubmit="return updateDescription(${index}, this)">
+  <input class="form-control input" name="newDescription" value="${tasks[index].description}">
   </form>`;
-  
-  updateLocal();
 };
 
 //Update description in task array
 const updateDescription = (index, form) => {
-  tasks[index].description = form.newDescription.value;
-  
-  updateLocal();
-  fillTodoList();
+  if (form.newDescription.value === "") {
+    wrongTaskInput(form.getElementsByTagName("input")[0]);
+    form.newDescription.value = tasks[0].description;
+    return false;
+  } else {
+    tasks[index].description = form.newDescription.value;
+    updateLocal();
+    fillTodoList();
+    return true;
+  }
 };
 
+//Add new style for empty inputs
+const wrongTaskInput = (imput) => {
+  console.log(imput);
+  imput.classList.add("wrongInput");
+  imput.placeholder = "Enter task name";
+  setTimeout(() => {
+    imput.classList.remove("wrongInput");
+  }, 1000);
+};
 
 //Refresh task list on reboot
 fillTodoList();
